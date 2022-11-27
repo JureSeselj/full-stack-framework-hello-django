9badd021ac7b216cfcd1b1541e71fba508c5ec3f
JureSeselj <jure.seselj@gmail.com>
2022-11-27 14:55:19 +0000

    Added missing development to settings.py file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dj_database_url
-import env
+from pathlib import Path
+if os.path.isfile("env.py"):
+    import env
+
+development = "DEVELOPMENT" in os.environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
